12e66a4aa6a4e4c6c74c946fbc33e1e0b2d9ec44
DidasMba <daniel.mbarushi@gmail.com>
2023-12-05 18:50:54 +0300

    save

--- lib/song.py
+++ lib/song.py
@@ -7,4 +7,21 @@ class Song:
 
 
 def create_table(cls):
-    
+    sql = """
+     CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+    """
+
+    CURSOR.execute(sql)
+
+  def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+        # Additional code to retrieve the last inserted ID and set it to self.id
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]  
